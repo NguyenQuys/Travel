a1b98ace618ca44021e4058f6d4335e1258e7d61
NguyenQuys <nguyenhuuquy110203@gmail.com>
2024-03-12 19:55:44 +0700

    QuyCommit_V1

---
+++ DuLich - Copy/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using DuLich.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DuLich.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataClasses1DataContext data = new DataClasses1DataContext("LAPTOP-ARKQSHH1\\KTEAM");
+        public ActionResult Index()
+        {
+            return View(data.Tinhs.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Index(string province, DateTime checkInDay, DateTime checkOutDay)
+        {
+            var findProvince = data.Tinhs.Where(m=>m.Province==province);
+            return View(findProvince);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DuLich - Copy/Models/FilterSearch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DuLich.Models
+{
+    public class FilterSearch
+    {
+        public string Province;
+        public DateTime checkInDay;
+        public DateTime checkOutDay;
+    }
+}
