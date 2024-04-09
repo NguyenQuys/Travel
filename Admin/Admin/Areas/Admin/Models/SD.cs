c35fc31f69c7cada2211d9964dd7c85433208ed0
nhutminhnguyen273 <nhutminhnguyen293@gmail.com>
2024-04-09 09:32:55 +0700

    remove Admin

--- Admin/Admin/Areas/Admin/Models/SD.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Admin.Areas.Admin.Models
-{
-    public static class SD
-    {
-        public const string Role_Customer = "Customer";
-        public const string Role_Visitor = "Visitor";
-        public const string Role_Admin = "Admin";
-    }
-}
--- Admin/Admin/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Admin.Models
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string CategoryName { get; set; }
-        public List<Tour>? Tours { get; set; }
-    }
-}
